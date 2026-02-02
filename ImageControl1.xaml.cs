1970ad274704a2509ab92caf3da24384c534cde1
taman.upadhaya <taman.upadhaya@Master-AE>
2026-02-02 23:07:05 +0000

    Add project files.

---
+++ ImageControl1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageViewer
+{
+    /// <summary>
+    /// Interaction logic for ImageControl1.xaml
+    /// </summary>
+    public partial class ImageControl1 : UserControl
+    {
+        public ImageControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void Prev_BTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Next_BTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
